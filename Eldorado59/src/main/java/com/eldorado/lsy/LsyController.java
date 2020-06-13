96b642d1a3f65af4e65e1d6a217da3bc71c68d54
CLNT3 <myuni9009@gmail.com>
2020-06-13 23:03:40 +0900

    feat: header, main(Proceeding)
    Signed-off-by: CLNT3 <myuni9009@gmail.com>

--- Eldorado59/src/main/java/com/eldorado/lsy/LsyController.java
+++ Eldorado59/src/main/java/com/eldorado/lsy/LsyController.java
@@ -2,18 +2,35 @@ package com.eldorado.lsy;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="lsy/*")
+@RequestMapping(value="Main/*")
 public class LsyController {
+	private static final
+	Logger logger = LoggerFactory.getLogger(LsyController.class);
 	
 	@RequestMapping(value = "test", method = RequestMethod.GET)
-	public String home_lyj(Locale locale, Model model) {
+	public String home(Locale locale, Model model) {
 	
 		return "home";
+	}
+
+	/*
+	 * @RequestMapping(value = "header", method = RequestMethod.GET) public String
+	 * header(Locale locale, Model model) { logger.info("/header 林家 角青");
+	 * 
+	 * return "lsy/header"; }
+	 */
+	@RequestMapping(value = "index", method = RequestMethod.GET)
+	public String main(Locale locale, Model model) {
+		logger.info("/main 林家 角青");
+		
+		return "lsy/main";
 	}
 }
---
+++ Eldorado59/src/main/webapp/WEB-INF/views/lsy/main.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+</head>
+<jsp:include page="../include/header.jsp"></jsp:include>
+<body>
+
+<article>
+<img src="https://movie-simg.yes24.com/NYes24//MgrMain//20/06/cats_085807.jpg/dims/optimize" alt="허남웅 영화경">
+</article>
+</body>  
+</html>
