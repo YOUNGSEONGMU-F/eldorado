f68e9615dd24d92a520f923d3004723001a8299c
harimKim <khl9566@naver.com>
2020-06-17 18:36:57 +0900

    commit - hr 뭔지모르겠음

--- Eldorado59/src/main/java/com/eldorado/khr/dao/KhrDAO.java
+++ Eldorado59/src/main/java/com/eldorado/khr/dao/KhrDAO.java
@@ -1,16 +1,5 @@
 package com.eldorado.khr.dao;
 
-import java.util.Map;
-
 public interface KhrDAO {
 
-	//회원정보수정
-	public int updatemember(Map<String, Object> user);
-	
-	//회원정보삭제
-	public int deleteMember(Map<String, Object> user);
-	
-	//마이페이지
-	
-	
 }
