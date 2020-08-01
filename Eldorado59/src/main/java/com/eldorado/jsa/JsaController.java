b7e634b5c2f1fc11ff954c1f77449186ad390f71
harimKim <khl9566@naver.com>
2020-08-01 21:04:38 +0900

    commit hr

--- Eldorado59/src/main/java/com/eldorado/jsa/JsaController.java
+++ Eldorado59/src/main/java/com/eldorado/jsa/JsaController.java
@@ -2,6 +2,7 @@ package com.eldorado.jsa;
 
 import java.util.Locale;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,11 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 @RequestMapping(value="MovieInfo/*")
 public class JsaController {
 	private static final
 	Logger logger = LoggerFactory.getLogger(JsaController.class);
+
 	@RequestMapping(value = "test", method = RequestMethod.GET)
 	public String home_lyj(Locale locale, Model model) {
 		return "home";
@@ -31,18 +34,17 @@ public class JsaController {
 		return "jsa/FestivalMovie";
 	}
 	
-	@RequestMapping(value = "NowPlayRank", method = RequestMethod.GET)
-	public String NowPlayRank(Locale locale, Model model) {
-		logger.info("/NowPlayRank 실행");
-		return "jsa/NowPlayRank";
-	}
+//	@RequestMapping(value = "NowPlayRank", method = RequestMethod.GET)
+//	public String NowPlayRank(Locale locale, Model model) {
+//		logger.info("/NowPlayRank 실행");
+//		return "jsa/NowPlayRank";
+//	}
 	
-	@RequestMapping(value = "Index", method = RequestMethod.GET)
-	public String Index(Locale local, Model model) {
-		logger.info("/Index 실행");
-		return "jsa/Index";
-	}
-	
+//	@RequestMapping(value = "Index", method = RequestMethod.GET)
+//	public String Index(Locale local, Model model) {
+//		logger.info("/Index 실행");
+//		return "jsa/Index";
+//	}
 	
 	
 	
